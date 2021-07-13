04ab4ca66a72d83c0d54bbbfe8ee5ba09d8a6e78
smzyuuuu <smzyuuuu@gmail.com>
2021-07-13 19:02:38 +0900

    Initial Commit

---
+++ InstaCrone/InstaCroneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCroneApp.swift
+//  InstaCrone
+//
+//  Created by Yuu Shimizu on 2021/07/13.
+//
+
+import SwiftUI
+
+@main
+struct InstaCroneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
